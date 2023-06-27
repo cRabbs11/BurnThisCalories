6b020cb2e7e8f004b06b7687cc991ae9814bf6ed
evgenykochkov <crabbs100@gmail.com>
2023-06-27 21:18:12 +0300

    поправил дизайн списка событий

--- app/src/main/java/com/ekochkov/burnthiscalories/view/holders/BurnEventHolder.kt
+++ app/src/main/java/com/ekochkov/burnthiscalories/view/holders/BurnEventHolder.kt
@@ -1,8 +1,8 @@
 package com.ekochkov.burnthiscalories.view.holders
 
 import androidx.recyclerview.widget.RecyclerView
+import com.ekochkov.burnthiscalories.R
 import com.ekochkov.burnthiscalories.data.entity.BurnEvent
-import com.ekochkov.burnthiscalories.data.entity.Product
 import com.ekochkov.burnthiscalories.databinding.ItemBurnEventBinding
 import com.ekochkov.burnthiscalories.util.Constants
 import com.ekochkov.burnthiscalories.util.OnItemClickListener
@@ -10,15 +10,28 @@ import com.ekochkov.burnthiscalories.util.OnItemClickListener
 class BurnEventHolder(val binding: ItemBurnEventBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(burnEvent: BurnEvent, clickListener: OnItemClickListener<BurnEvent>) {
-        binding.status.text = when(burnEvent.eventStatus) {
-            Constants.BURN_EVENT_STATUS_DONE -> Constants.BURN_EVENT_STATUS_DONE_TEXT
-            Constants.BURN_EVENT_STATUS_IN_PROGRESS -> Constants.BURN_EVENT_STATUS_IN_PROGRESS_TEXT
-            else -> {Constants.BURN_EVENT_STATUS_NOT_FOUND_TEXT}
+        when(burnEvent.eventStatus) {
+            Constants.BURN_EVENT_STATUS_DONE -> {
+                binding.statusIcon.setImageResource(R.drawable.ic_baseline_done_24)
+                binding.statusText.text = Constants.BURN_EVENT_STATUS_DONE_TEXT
+            }
+            Constants.BURN_EVENT_STATUS_IN_PROGRESS -> {
+                binding.statusIcon.setImageResource(R.drawable.ic_baseline_loop_24)
+                binding.statusText.text = Constants.BURN_EVENT_STATUS_IN_PROGRESS_TEXT}
+            else -> {
+                binding.statusIcon.setImageResource(R.drawable.ic_baseline_loop_24)
+                binding.statusText.text = Constants.BURN_EVENT_STATUS_NOT_FOUND_TEXT
+            }
         }
-        val caloriesBurned = "сожжено калорий: ${burnEvent.caloriesBurned}"
-        val countOfProducts = "продуктов: ${burnEvent.productsId.size}"
-        binding.caloriesBurned.text = caloriesBurned
+        val countOfProducts = "${burnEvent.productsId.size} продуктов"
+        var calories = 0
+        burnEvent.productsId.forEach {
+            calories+=it.calory
+        }
+        val allCalories = "${burnEvent.caloriesBurned}/$calories ккал"
+
         binding.countOfProducts.text = countOfProducts
+        binding.allCalories.text = allCalories
 
         binding.root.setOnClickListener {
             clickListener.onItemClick(burnEvent)

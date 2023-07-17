fcd3618773cf99d634cbe526d12524947a94591f
evgenykochkov <crabbs100@gmail.com>
2023-07-17 19:04:24 +0300

    убрал лишний код

--- app/src/main/java/com/ekochkov/burnthiscalories/viewModel/MainActivityViewModel.kt
+++ app/src/main/java/com/ekochkov/burnthiscalories/viewModel/MainActivityViewModel.kt
@@ -1,10 +1,8 @@
 package com.ekochkov.burnthiscalories.viewModel
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ekochkov.burnthiscalories.App
-import com.ekochkov.burnthiscalories.data.entity.Profile
 import com.ekochkov.burnthiscalories.domain.Interactor
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -12,16 +10,12 @@ import javax.inject.Inject
 
 class MainActivityViewModel: ViewModel() {
 
-    val profileLiveData = MutableLiveData<Profile?>()
 
     @Inject
     lateinit var interactor: Interactor
 
     init {
         App.instance.dagger.inject(this)
-        viewModelScope.launch(Dispatchers.IO) {
-            profileLiveData.postValue(interactor.getProfile())
-        }
     }
 
     fun tryLaunchStepCount() {

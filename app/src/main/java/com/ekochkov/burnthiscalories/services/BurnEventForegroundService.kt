b3e0acfd2f28b95aafa1aa3b474b384029c300b2
evgenykochkov <crabbs100@gmail.com>
2023-08-17 19:36:16 +0300

    сделал отслеживание количества шагов в foreground сервисе

--- app/src/main/java/com/ekochkov/burnthiscalories/services/BurnEventForegroundService.kt
+++ app/src/main/java/com/ekochkov/burnthiscalories/services/BurnEventForegroundService.kt
@@ -2,6 +2,10 @@ package com.ekochkov.burnthiscalories.services
 
 import android.app.*
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Build
 import android.os.IBinder
 import androidx.annotation.RequiresApi
@@ -9,14 +13,17 @@ import androidx.annotation.RequiresApi
 import com.ekochkov.burnthiscalories.R
 import com.ekochkov.burnthiscalories.util.Constants
 
-class BurnEventForegroundService: Service() {
+class BurnEventForegroundService: Service(), SensorEventListener {
 
+    lateinit var sensorManager : SensorManager
+    lateinit var sensor: Sensor
     lateinit var notificationManager: NotificationManager
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         createNotificationChannel()
         startForeground(1, getNotification(Constants.BURN_EVENT_IS_RUNNING, "ккал осталось..."))
+        startSensor()
         return START_NOT_STICKY
     }
 
@@ -36,6 +43,18 @@ class BurnEventForegroundService: Service() {
         }
     }
 
+    private fun startSensor() {
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    private fun stopSensor() {
+        sensorManager?.let {
+            it.unregisterListener(this)
+        }
+    }
+
     @RequiresApi(Build.VERSION_CODES.O)
     private fun getNotification(title: String, text: String): Notification {
         val notification: Notification = Notification.Builder(this, Constants.BURN_EVENT_SERVICE_CHANNEL_ID)
@@ -44,5 +63,14 @@ class BurnEventForegroundService: Service() {
             .setSmallIcon(R.drawable.ic_baseline_fitness_center_24)
             .build()
         return notification
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onSensorChanged(event: SensorEvent?) {
+        val value = event!!.values[0].toInt()
+        notificationManager.notify(1, getNotification(Constants.BURN_EVENT_IS_RUNNING, "ккал осталось...$value"))
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
     }
 }

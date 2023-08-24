c6a7228a992a5f1d885e6e628fe796c51062ebdb
evgenykochkov <crabbs100@gmail.com>
2023-08-24 14:34:41 +0300

    сделал возможность продолжить burnEvent, если он был остановлен

--- app/src/main/java/com/ekochkov/burnthiscalories/viewModel/BurnEventFragmentViewModel.kt
+++ app/src/main/java/com/ekochkov/burnthiscalories/viewModel/BurnEventFragmentViewModel.kt
@@ -6,14 +6,18 @@ import androidx.lifecycle.viewModelScope
 import com.ekochkov.burnthiscalories.App
 import com.ekochkov.burnthiscalories.data.entity.BurnEvent
 import com.ekochkov.burnthiscalories.domain.Interactor
+import com.ekochkov.burnthiscalories.util.Constants
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class BurnEventFragmentViewModel(private val burnEventId: Int): ViewModel() {
 
     val burnEventLiveData = MutableLiveData<BurnEvent>()
+    val isBurnEventServiceCanRun = MutableLiveData<Boolean>()
 
     @Inject
     lateinit var interactor: Interactor
@@ -22,8 +26,11 @@ class BurnEventFragmentViewModel(private val burnEventId: Int): ViewModel() {
         App.instance.dagger.inject(this)
 
         viewModelScope.launch(Dispatchers.IO) {
-            interactor.getBurnEventFlow(burnEventId).collect{
-                burnEventLiveData.postValue(it)
+            this.launch {
+                combine(interactor.getBurnEventFlow(burnEventId), interactor.getIsBurnEventServiceIsRunningStateFlow()) { burnEvent, isServiceRunning ->
+                    burnEventLiveData.postValue(burnEvent)
+                    isBurnEventServiceCanRun.postValue(burnEvent?.eventStatus==Constants.BURN_EVENT_STATUS_IN_PROGRESS && !isServiceRunning)
+                }.collect()
             }
         }
     }
